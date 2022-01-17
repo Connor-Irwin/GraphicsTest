fd6532873009452e5f71c74c15d8451c54e50391
Connor-Irwin <connorai2004@gmail.com>
2022-01-17 17:23:24 -0600

    x_move = rand() % width; y_move = rand() % height;

--- Graphics Test/Graphics/CrawlingAnt.cpp
+++ Graphics Test/Graphics/CrawlingAnt.cpp
@@ -5,75 +5,38 @@ CrawlingAnt::CrawlingAnt(int window_width, int window_height, SDL_Renderer* rend
     setWindowSize(window_width, window_height);
     setRenderer(rend);
 
-    x = window_width / 2;
-    y = window_height / 2;
+    x = width / 2;
+    y = height / 2;
 
-    edginess = 1;
+    x_move = x;
+    y_move = y;
+
+    edginess = 10;
 
 	fillScreen();
 }
 
 void CrawlingAnt::update() {
+    /*
     dist_from_center = (int)distance(x, y, width / 2, height / 2);
     dist_from_right_edge = (int)distance(x, y, width, y);
     dist_from_left_edge = (int)distance(x, y, 0, y);
     dist_from_bottom_edge = (int)distance(x, y, x, height);
     dist_from_top_edge = (int)distance(x, y, x, 0);
+    */
 
-    direction = rand() % 5;
+    x_move = rand() % width;
+    y_move = rand() % height;
+    
+    if (SDL_abs(x_move - width / 2) < width / 2 / edginess) x_move = rand() % width;
+    if (SDL_abs(y_move - height / 2) < height / 2 / edginess) y_move = rand() % height;
 
-    int x_move = rand() % width;
-    int y_move = rand() % height;
+    if (x_move > x) x += 2;
+    else if (x_move < x) x -= 2;
+    
+    if (y_move > y) y += 2;
+    else if (y_move < y) y -= 2;
 
-    switch (direction) {
-    case 1: { // Right
-        if (dist_from_right_edge < dist_from_center / edginess || x > width) direction = rand() % 5;
-
-        x++;
-        drawPoint(x, y, 255, 178, 0);
-        x++;
-        drawPoint(x, y, 255, 178, 0);
-
-        break;
-    }
-
-    case 2: { // Left
-        if (dist_from_left_edge < dist_from_center / edginess || x < 0) direction = rand() % 5;
-
-        x--;
-        drawPoint(x, y, 255, 178, 0);
-        x--;
-        drawPoint(x, y, 255, 178, 0);
-
-        break;
-    }
-
-    case 3: { // Down
-        if (dist_from_bottom_edge < dist_from_center / edginess || y > height) direction = rand() % 5;
-
-        y--;
-        drawPoint(x, y, 255, 178, 0);
-        y--;
-        drawPoint(x, y, 255, 178, 0);
-
-        break;
-    }
-
-    case 4: { // Up
-        if (dist_from_top_edge < dist_from_center / edginess || y < 0) direction = rand() % 5;
-
-        y++;
-        drawPoint(x, y, 255, 178, 0);
-        y++;
-        drawPoint(x, y, 255, 178, 0);
-
-        break;
-    }
-
-    default: {
-        break;
-    }
-    }
-
+    drawPoint(x, y, 255, 178, 0);
     present();
 }
--- Graphics Test/Graphics/CrawlingAnt.h
+++ Graphics Test/Graphics/CrawlingAnt.h
@@ -6,6 +6,7 @@ class CrawlingAnt : public Graphics {
 private:
 	int x, y;
 
+    /*
     int dist_from_center,
         dist_from_right_edge,
         dist_from_left_edge,
@@ -13,6 +14,11 @@ private:
         dist_from_top_edge;
 
     int direction;      // The direction the ant will move next (0 for no movement, 1 for right, 2 for left, 3 for down, 4 for up).
+    */
+
+    int x_move, // The x coordinate to move towards.
+        y_move; // The y coordinate to move towards.
+
     double edginess;    // How close to the edge the ant will get.
 
 public:
